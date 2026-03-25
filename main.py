94e64233e4208d0339de229f3dfad9a9b464de66
StychukMaria <stychuk.maria@gmail.com>
2026-03-25 21:29:52 +0200

    Solution2

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from db.models import Genre, Actor
 def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
     actors = [
-        ["George", "Klooney"],
-        ["Kianu", "Reaves"],
-        ["Scarlett", "Keegan"],
-        ["Will", "Smith"],
-        ["Jaden", "Smith"],
-        ["Scarlett", "Johansson"]
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
     ]
     for genre in genres:
         Genre.objects.create(name=genre)
@@ -22,7 +22,8 @@ def main() -> QuerySet:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    (Actor.objects.filter(first_name="George", last_name="Klooney").
+     update(last_name="Clooney"))
     (Actor.objects.filter(first_name="Kianu", last_name="Reaves").
      update(first_name="Keanu", last_name="Reeves"))
 
